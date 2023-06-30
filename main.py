ea49ad257a1eb852be7453b0f1a81fa833d32feb
Natallia Shybeka <n.shybeka@gmail.com>
2023-06-30 13:59:55 +0400

    add Getting the Contact Form

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 from flask import Flask, render_template, request
+import requests
+
+posts = requests.get("https://api.npoint.io/31247fffda98a6fdd654").json()
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# @app.route("/form-entry", methods=["POST"])
+# def receive_data():
+#     data = request.form
+#     print(data["name"])
+#     print(data["email"])
+#     print(data["phone"])
+#     print(data["message"])
+#     return "<h1>Successfully sent your message</h1>"
 
 
-@app.route("/login", methods=["POST"])
-def receive_data():
-    name = request.form["username"]
-    password = request.form["password"]
-    return f"<h1>Name: {name}, Password: {password}</h1>"
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
